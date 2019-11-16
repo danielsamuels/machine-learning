b4e21785dd246d485a690aee1919bcf30f58d1a6
Daniel Samuels <daniel.samuels1@gmail.com>
2019-11-16 00:13:06 +0000

    Delete some old files

--- baconbot-cnn/setup.sh
+++ baconbot-cnn/setup.sh
@@ -1,5 +1,3 @@
 . .venv/Scripts/activate
 python -m pip install --upgrade pip
-pip install tensorflow-gpu pyyaml h5py numpy Pillow scipy --ignore-installed
-pip install jupyterlab jupyter_http_over_ws
-jupyter serverextension enable --py jupyter_http_over_ws
+pip install tensorflow-gpu pyyaml h5py numpy Pillow scipy
